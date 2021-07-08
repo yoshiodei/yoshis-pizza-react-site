d4e01247cc7adc5f22a751ba3e9d00d7e7f65b38
yoshiodei <yoshiodei@yahoo.com>
2021-07-08 21:27:28 +0000

    modify App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <section className="header">
-        <nav className="header__nav">
-            <h3 className="header__brand" title="The best online pizza shop.">yoshi's Pizza
-            </h3>
-            <div className="header__toggle-btn">
-                <span className="header__toggle-btn__bars"></span>
-                <span className="header__toggle-btn__bars"></span>
-                <span className="header__toggle-btn__bars"></span>                
-            </div>
-            <ul className="header__list">
-                <li className="header__list-item">
-                    <a href="">log in</a>
-                </li>
-                <li className="header__list-item">
-                    <a href="">sign up</a>
-                </li>
-            </ul>
-        </nav>
-    </section>
+      <Header />
       <section className="main">
           <div className="main__ads">
               <div className="main__ads__ad"></div>
